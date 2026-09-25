1db8852fe3b80e87b4d24e74e4cae4c571e395c3
Adrita Das <adrita.das2020@gmail.com>
2026-09-25 12:10:35 +0600

    Add loginservice

--- OnlineShoppingSystem/src/main/java/service/LoginService.java
+++ OnlineShoppingSystem/src/main/java/service/LoginService.java
@@ -4,24 +4,22 @@ import database.UserDAO;
 
 public class LoginService {
 
+    private final UserDAO dao;
+
+    public LoginService(UserDAO dao) {
+        this.dao = dao;
+    }
 
     public boolean validateLogin(String email, String password) {
 
-
-        if(email == null || email.isEmpty()) {
+        if (email == null || email.isEmpty()) {
             return false;
         }
 
-
-        if(password == null || password.isEmpty()) {
+        if (password == null || password.isEmpty()) {
             return false;
         }
-
-
-        UserDAO dao = new UserDAO();
 
         return dao.loginUser(email, password);
-
     }
-
 }
