af26fae60159d1009efd97382075bd3ec159b6ce
Adrita Das <adrita.das2020@gmail.com>
2026-09-25 12:13:54 +0600

    login mockito test

--- OnlineShoppingSystem/src/test/java/test/LoginTest.java
+++ OnlineShoppingSystem/src/test/java/test/LoginTest.java
@@ -1,70 +1,54 @@
 package test;
 
+import database.UserDAO;
 import org.junit.jupiter.api.Test;
 import service.LoginService;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.*;
 
 public class LoginTest {
 
-
     @Test
-    void validLoginTest(){
+    void validLoginTest() {
 
+        String email = "adrita@gmail.com";
+        String password = "Test1234";
 
-        LoginService service = new LoginService();
+        UserDAO dao = mock(UserDAO.class);
 
+        when(dao.loginUser(email, password))
+                .thenReturn(true);
+
+        LoginService service = new LoginService(dao);
 
         boolean result =
-                service.validateLogin(
-                        "rahim2@gmail.com",
-                        "12345"
-                );
-
+                service.validateLogin(email, password);
 
         assertTrue(result);
 
+        verify(dao).loginUser(email, password);
     }
-
 
 
     @Test
-    void invalidPasswordTest(){
+    void invalidLoginTest() {
 
+        String email = "adrita02@gmail.com";
+        String password = "12345678a";
 
-        LoginService service = new LoginService();
+        UserDAO dao = mock(UserDAO.class);
 
+        when(dao.loginUser(email, password))
+                .thenReturn(false);
+
+        LoginService service = new LoginService(dao);
 
         boolean result =
-                service.validateLogin(
-                        "rahim2@gmail.com",
-                        "wrong123"
-                );
-
+                service.validateLogin(email, password);
 
         assertFalse(result);
 
+        verify(dao).loginUser(email, password);
     }
-
-
-
-    @Test
-    void emptyEmailTest(){
-
-
-        LoginService service = new LoginService();
-
-
-        boolean result =
-                service.validateLogin(
-                        "",
-                        "12345"
-                );
-
-
-        assertFalse(result);
-
-    }
-
 }
